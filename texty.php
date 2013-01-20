7c92e6b52f4cc1628684687a9e9c735a45b15b82
SoulOfSet <awesomewhitehot@yahoo.com>
2013-01-20 12:46:11 -0600

    Update texty.php
    Not Done

--- texty.php
+++ texty.php
@@ -1,3 +1,42 @@
 #!/usr/bin/php
  <?php
  //Script by SoulOfSet
+ 
+ //Prequisites For The Mail Function
+ $textAddress = "number@e-mailaddress.com"; //You could use an email or cell number here. Examples: http://www.emailtextmessages.com/
+ $textSubject = "Armagetron Message";
+ $textHeaders = 'From: root@myserver.com'."\r\n".
+                'Reply-To: root@myserver.com'."\r\n";
+ 
+ function sendMessage($textMessage, $type, $personalMessage, $sender)
+  {
+   if($personalMessage == 1) { $textSubject = "Personal Message"; }
+   $mail = mail($textAddress, $textSubject, $textMessage, $headers);
+   if(!mail)
+    {
+      echo "Something is broken with the mail function.\n";
+    }
+   elseif((mail) && $personalMessage == 1)
+   {
+     print("player_message $sender \"Message Sent!\"\n");
+   }
+   $textSubject = "Armagetron Message"; //Please make sure this is the same as the original $textMessage near the top.
+  }
+  //
+  
+  //Events which you wish to receive a message
+  $onChat = 0;
+  $onKill = 0;
+  $onAdminCommand = 0;
+  $onPlayerBan = 0;
+  $onPlayerKick = 0;
+  
+   
+   
+   
+   
+     while(1)
+      {
+       $input = rtrim(fgets(STDIN, 1024)); 
+     		$param = explode(" ", $input);
+   
