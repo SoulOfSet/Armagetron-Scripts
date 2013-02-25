57e96cb5a6ef3ad361f769736861a56d84d4d484
SoulOfSet <awesomewhitehot@yahoo.com>
2013-02-25 09:52:05 -0800

    Update whois.php

--- whois.php
+++ whois.php
@@ -1,3 +1,19 @@
 #!/usr/bin/php
- <?php
-    //Script by SoulOfSet
+<?php
+//Script by SoulOfSet
+/*Script which, on new player enter, will log IP with their name and record all future
+names under that addy to the same ip.*/
+
+ $sWhoisFile = "whois.txt"
+ $aWhoisFile = fopen($sWhoIsFile, 'w+'); //Open for reading and writing, if it does not exist it will try and create it
+
+ while (1)
+  {
+		 $input = rtrim(fgets(STDIN, 1024));
+			$param = explode(" ", $input); 
+   
+   
+   
+   
+   
+   
