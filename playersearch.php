d8be9fb8466b91d19ab08ed2fe0d2f23023366c6
moofie <marktoeniskoetter@gmail.com>
2013-03-02 12:33:30 -0500

    Update playersearch.php

--- playersearch.php
+++ playersearch.php
@@ -2,36 +2,26 @@
 <?php
 //script by moofie
 $filename = "/home/mark/armagetronad/servers/moofie_demo/var/ladderlog.txt";
-$handle = fopen($filename, "rb");
-$contents = fread($handle, filesize($filename));
-$records = explode(" ", $contents);
-fclose($handle);
-while(1)
-  {
-		$line = rtrim(fgets(STDIN, 1024));
-		$param = explode(" ", $line);
-			if(preg_match("PLAYER_ENTERED", $handle))
-			{
-				$records[2] = $playerip;
-				$records[3] = $playername;
-			}
-			if(preg_match("/^INVALID_COMMAND/", $line))
-			{
-				if($param[1] == "/whois")
-				{
-					if($param[5] == $playername)
-					{
-						print("player_message {$param[2]} \"{$playername}'s IP is {$playerip}.  His other aliases are {$playername}.\"\n");
-					}
-					elseif($param[5] == $playerip)
-						{
-						print("player_message {$param[2]} \"That IP belongs to {$playername}\"\n");
-						}
-					else
-						{
-						print("player_message {$param[2]} \"That user is not in our records\"\n");
-						}
-				}
-			}
-	}
+$regex = array(
+  'find' => array(
+   '/\s+/',
+   '/\:/',
+  ),
+  'replace' => array(
+   '_',
+   '\:'
+  )
+ );
+while (!feof(STDIN))
+{
+	$param = rtrim(fgets(STDIN));
+		if($param[0] == 'INVALID_COMMAND' && $param[1] == "/whois" && $param[4] <= 1)
+		{
+			if(filter_var($param[5], FILTER_VALIDATE_IP))
+		}		
+		else
+		{
+			$user = preg_replace($regex['find'], $regex['replace'], $param[5]);
+		}
+}
 ?>
