450e2286e3799496abcb7fb70d8cd3ef6fbc7e25
moofie <marktoeniskoetter@gmail.com>
2013-03-02 12:49:02 -0500

    Update playersearch.php

--- playersearch.php
+++ playersearch.php
@@ -18,10 +18,7 @@ while (!feof(STDIN))
 		if($param[0] == 'INVALID_COMMAND' && $param[1] == "/whois" && $param[4] <= 1)
 		{
 			$ip = filter_var($param[5], FILTER_VALIDATE_IP) ? true : false;
-				{
-					$user = preg_replace($regex['find'], $regex['replace'], $param[5]);
-				}
+			$fp = fopen($file, 'r');
 		}		
-		
 }
 ?>
