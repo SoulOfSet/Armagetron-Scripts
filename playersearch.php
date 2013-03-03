9ff4b46f0063a6f653c3cad14fd9dac591f5ac69
moofie <marktoeniskoetter@gmail.com>
2013-03-03 09:45:41 -0500

    Update playersearch.php

--- playersearch.php
+++ playersearch.php
@@ -1,63 +1,84 @@
 #!/usr/bin/php
 <?php
-//script by moofie
-$filename = "/home/mark/armagetronad/servers/moofie_demo/var/ladderlog.txt";
+
+$file = "/home/mark/armagetronad/servers/moofie_demo/var/ladderlog.txt";
+
 $regex = array(
-  'find' => array(
-   '/\s+/',
-   '/\:/',
-  ),
-  'replace' => array(
-   '_',
-   '\:'
-  )
- );
+	'find' => array(
+		'/\s+/',
+		'/\:/',
+	),
+	'replace' => array(
+		'_',
+		'\:'
+	)
+);
+
 while (!feof(STDIN))
 {
 	$param = explode(" ", rtrim(fgets(STDIN)));
-		if($param[0] == 'INVALID_COMMAND' && $param[1] == "/whois" && $param[4] <= 1)
+	
+	if($param[0] == 'INVALID_COMMAND')
+	{
+		if ($param[1] == "/whois" && $param[4] <= 1)
 		{
+			$info = array();
 			$ip = filter_var($param[5], FILTER_VALIDATE_IP) ? true : false;
+			
+			if (!$ip)
+				$param[5] = preg_replace($regex['find'], $regex['replace'], strtolower($param[5]));
+			
 			$fp = fopen($file, 'r');
-				while (!feof($fp))
-				{
+			
+			while (!feof($fp))
+			{
 				$log = explode(" ", rtrim(fgets($fp)));
-					if($log[0] == "PLAYER_ENTERED")
+				
+				if ($log[0] == "PLAYER_ENTERED")
+				{
+					if ($ip)
 					{
-						if($ip)
-                            {
-                            $info = array();
-								if($log[2] == $param[5] && !in_array($log[3], $info))
-								$info[] = $log[1];
-                            }
-                        else
-							{
-							
-							}
-							
+						if ($log[2] == $param[5] && !in_array($log[1], $info))
+							$info[] = $log[1];
 					}
-					elseif ($log[0] == "PLAYER_RENAMED")
+					else
 					{
-						if($ip)
-                            {
-                             if($log[3] == $param[5])
-                                {
-                                    if (!in_array($log[1], $info))
-                                        $info[] = $log[1];
-                                                       
-                                    if (!in_array($log[2], $info))
-                                        $info[] = $log[2];
-                                }                  
-                            }
-                        else
-							{
-							
-							}
+						if ($log[1] == $param[5] && !in_array($log[2], $info))
+							$info[] = $log[2];
 					}
-					
 				}
+				elseif ($log[0] == "PLAYER_RENAMED")
+				{
+					if ($ip)
+					{
+						if ($log[3] == $param[5])
+						{
+							if (!in_array($log[1], $info))
+								$info[] = $log[1];
+							
+							if (!in_array($log[2], $info))
+								$info[] = $log[2];
+						}
+					}
+					else
+					{
+						if (($log[1] == $param[5] || $log[2] == $param[5]) && !in_array($log[3], $info))
+							$info[] = $log[3];
+					}
+				}
+			}
+			
 			fclose($fp);
-			print("PLAYER_MESSAGE {$param[2]} \"0x0000ccWhois0xffffff: " . implode('0x0000cc, 0xffffff', $info) . "\"\n");
-		}		
+			
+			if (!empty($info))
+				$message = str_replace('"', '\"', implode('0x0000cc, 0xffffff', $info));
+			else
+				$message = "Search returned nothing for '{$param[5]}'.";
+			
+			print("PLAYER_MESSAGE {$param[2]} \"0x0000ccWhois0xffffff: {$message}\"\n");
+		}
+	}
 }
+
 ?>
+
