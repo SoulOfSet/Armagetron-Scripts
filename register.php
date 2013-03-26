eb39e1cf8a4b6ae215d175398ac6a699d3fd3fad
moofie <marktoeniskoetter@gmail.com>
2013-03-25 19:40:40 -0500

    Create register.php

---
+++ register.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+mysql_connect("localhost", "root", "51063t") or die(mysql_error());
+mysql_select_db("auth") or die(mysql_error());
+while(!feof(STDIN))
+{
+      $input = rtrim(fgets(STDIN, 1024));
+		$param = explode(" ", $input); 
+	
+	if(preg_match("/^INVALID_COMMAND/", $input))
+	{
+		$param[5] = $username;
+		$param[6] = $password;
+		$password = md5($password);
+		mysql_query("INSERT INTO admin(username, passcode) VALUES('$username', '$password');");
+	}
+
+}
+?>
