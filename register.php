e5b0acfcfd63e010d89ca183a9e6de1e185158ae
moofie <marktoeniskoetter@gmail.com>
2013-03-26 17:52:38 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,19 +1,38 @@
 #!/usr/bin/php
 <?php
-mysql_connect("localhost", "root", "******") or die(mysql_error());
+mysql_connect("localhost", "root", "*****") or die(mysql_error());
 mysql_select_db("auth") or die(mysql_error());
+$registered = array();
 while(!feof(STDIN))
 {
-      $input = rtrim(fgets(STDIN, 1024));
+    	$input = rtrim(fgets(STDIN, 1024));
 		$param = explode(" ", $input); 
 	
 	if(preg_match("/^INVALID_COMMAND/", $input))
 	{
-		$param[5] = $username;
-		$param[6] = $password;
-		$password = md5($password);
-		mysql_query("INSERT INTO admin(username, passcode) VALUES('$username', '$password');");
+		if($param[1] == "/register")
+		{
+			if(in_array($param[3], $registered))
+			{
+				print("console_message Sorry, you have already registered for a Shenanaboosh ID.\n");
+			}
+			else
+			{
+				$username = $param[5];
+				$password = $param[6];
+				$md5pw = md5($password);
+				mysql_query("INSERT INTO admin(username, passcode) VALUES('$username', '$md5pw');");
+				print("console_message Thanks for registering!  You can now login by typing '/login {$username}@shenanaboosh.me' with {$password} as your password.\n");
+				$registered[] = $param[3];
+			}
+		}
+		else
+		{
+		print("console_message That command does not exist.\n");
+		}
 	}
 
 }
+mysql_close();
 ?>
+
