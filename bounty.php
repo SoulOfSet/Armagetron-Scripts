463b722392b26abd2e7a479035ca38a893df3a6d
moofie <marktoeniskoetter@gmail.com>
2013-03-27 17:23:14 -0500

    Update bounty.php

--- bounty.php
+++ bounty.php
@@ -2,22 +2,26 @@
 <?php
 //script by moofie
 //note: not currently working
-while(true)
 $players = array();
+while (!feof(STDIN))
 {
     $input = rtrim(fgets(STDIN, 1024));
-  $param = explode(" ", $input); 
-	if(preg_match("/^ONLINE_PLAYER/", $input))
+	$param = explode(" ", $input); 
+		if(preg_match("/^PLAYER_ENTERED/", $input))
 		{
 			$name = $param[1];
-			$players[] = $name;
-			
+			$players[] = $name;	
 		}
-	if(preg_match("/^NEW_ROUND/", $input))
+		if(preg_match("/^NEW_ROUND/", $input))
 		{
-		$bounty = array_rand($players);
-		print("console_message There has been a bounty placed on {$bounty}'s head for 2 points!");
-		shuffle($players);
+			$bounty = array_rand($players);
+			print("console_message There has been a bounty placed on {$bounty}'s head for 2 points!");
+			shuffle($players);
+		}
+		if(preg_match("/^PLAYER_LEFT/", $input))
+		{
+			$remove = array_search($param[1], $players);
+			unset($players[$remove]);
 		}
 }
 ?>
