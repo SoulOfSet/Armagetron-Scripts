e22327f7c04432cbfef577acc37db6b7af145eac
moofie <marktoeniskoetter@gmail.com>
2013-03-27 23:15:48 -0500

    Update bounty.php

--- bounty.php
+++ bounty.php
@@ -1,27 +1,39 @@
 #!/usr/bin/php
 <?php
-//script by moofie
-//note: not currently working
+
+// Options
+$username = "0xff0000Bounty";
+$points = rand(1, 5);
+
+// Ignore These
+$bounty  = "";
 $players = array();
+
 while (!feof(STDIN))
 {
-    $input = rtrim(fgets(STDIN, 1024));
-	$param = explode(" ", $input); 
-		if(preg_match("/^PLAYER_ENTERED/", $input))
-		{
-			$name = $param[1];
-			$players[] = $name;	
-		}
-		if(preg_match("/^NEW_ROUND/", $input))
-		{
-			$bounty = array_rand($players);
-			print("console_message There has been a bounty placed on {$bounty}'s head for 2 points!");
-			shuffle($players);
-		}
-		if(preg_match("/^PLAYER_LEFT/", $input))
-		{
-			$remove = array_search($param[1], $players);
-			unset($players[$remove]);
-		}
+    $input = rtrim(fgets(STDIN));
+	$param = explode(" ", $input);
+	
+	if ($param[0] == "CYCLE_CREATED")
+	{
+		array_push($players, $param[1]);
+	}
+	elseif ($param[0] == "NEW_ROUND")
+	{
+		$players = array();
+	}
+	elseif ($param[0] == "GAME_TIME" && $param[1] == "0")
+	{
+		shuffle($players);
+		$bounty  = array_shift($players);
+		
+		print("console_message {$username}0xffff7f: There's a bounty on {$bounty}'s head for {$points} points!  Get him!" . "\n");
+	}
+	elseif ($param[0] == "DEATH_FRAG" && $param[1] == $bounty)
+	{
+		print("add_score_player {$param[2]} {$points}" . "\n");
+		print("console_message {$username}0xffff7f: {$param[2]} killed {$bounty} for {$points} points!" . "\n");
+	}
 }
+
 ?>
