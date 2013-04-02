56bf8777466beeb7ffb1f62b6593efe0262dbede
SoulOfSet <awesomewhitehot@yahoo.com>
2013-04-02 12:52:13 -0700

    Update script1.php
    More updates to dialogue

--- adventures/roguedz/script1.php
+++ adventures/roguedz/script1.php
@@ -11,15 +11,25 @@
    //Not the shorthand one but the full title. (Ex. The Prince To The Rescue instead of prince_rescue or w/e)
    $sAdventureTitle = "The Rogue DeathZone"
    
-   $aConceredDialog = 
+   $bPhylisDialoguePassed = FALSE;
    
    //Settings for this particular script
    $aSettings = array("cycle_speed 1", "sp_walls_length 1", "walls_length 1", "cycle_walls_length 1", "cycle_brake_deplete 0", "cycle_speed_decay_above 10", "cycle_delay 0.1", "cycle_rubber 1000");
    //We're using zones as NPC's here. Start from type unless they need names. (Ex. n Phylis death 10 0 etc)
    //Some may have paths to help make it more realistic
-   $aNpcSpawns = array("n Phylis rubber 310 400 15 0 0 0 0 true 1 0 0 15", "n Sarah rubber 150 300 15 0 0 0 0 true 1 0 1 15", "n Vain rubber 300 200 15 0 0 0 0 true 1 0.3 0", "n Jack 327 100 7 0 0 0 0 true 0 1 0 7", "n Carol rubber 200 100 15 0 0 0 0 true 0.4 0 1 15", "n Moofie 250 450 10 0 0 0 0 true 0.4 0.9 0.4 15");
+   $aNpcSpawns = array("n Phylis rubber 310 400 15 0 0 0 0 true 1 0 0 15", "n Sarah rubber 150 300 15 0 0 0 0 true 1 0 1 15", "n Vain rubber 300 200 15 0 0 0 0 true 1 0.3 0", "n Jack 327 100 7 0 0 0 0 true 0 1 0 7", "n Rolan rubber 200 100 15 0 0 0 0 true 0.4 0 1 15", "n Moofie 250 450 10 0 0 0 0 true 0.4 0.9 0.4 15");
+   //Dialogue for the NPC's after the initial chat with phylis is cleared
+   //A set for each so it doesn't seem half assed
+   //Sarah
+   $aSarahDialouge = array("DeathZone has gone too far with this", "I cant beleive this is happening", "He even took some of our members with him.", "Vain is taking this rather harshly. They were close. You should talk to him");
+   //Vain
+   $aVainDialouge = array("...", "Death....why?", "To think something like this..in our clan.", "You should go i'm not much for talking right now");
+   //Jack
+   $aJackDialouge = array("Look are some things you should know about Vain. He took several of our members and is skilled with death magic. One hit of of it will desroy you, watch yourself.", "Here I thought he was the mysterious type. Who would of that Mr. Secretive would do something like this. We don't even know his full name", "As his former clan members we are responsible for this");
+   //Rolan
+   $aRolanDialouge = array("Fuckin Death needs to get his shit together", "Who the fuck does he think he is", "Get him back man. Fuckin last thing we need is this on our rep", "Bullshit Death....bullshit");
+   //The moofie dialouge
    
-   $aPhylisDialogue = array
    
    while (!feof(STDIN))
     {
