4c2b72b64aead8a18937b6cf805f36b2d85d0593
moofie <marktoeniskoetter@gmail.com>
2013-04-09 20:44:37 -0500

    Update jumbosumo.php

--- jumbosumo.php
+++ jumbosumo.php
@@ -24,25 +24,28 @@ while (!feof(STDIN))
 			unset($players[$oldname]);
 			array_push($players, $param[2]);
 		}
-		if ($numplayers <= 3)
+		if ($param[0] == "GAME_TIME" && $param[1] == "0")
 		{
-		print("SPAWN_ZONE sumo 150 150 {$zonesizes[1]} 0 0\n");
-		}
-		if ($numplayers >= 4 && $numplayers <= 6)
-		{
-		print("SPAWN_ZONE sumo 150 150 {$zonesizes[2]} 0 0\n");
-		}
-		if ($numplayers >= 7 && $numplayers <= 9)
-		{
-		print("SPAWN_ZONE sumo 150 150 {$zonesizes[3]} 0 0\n");
-		}
-		if ($numplayers >= 10 && $numplayers <= 12)
-		{
-		print("SPAWN_ZONE sumo 150 150 {$zonesizes[4]} 0 0\n");
-		}
-		if ($numplayers >= 13 && $numplayers <= 15)
-		{
-		print("SPAWN_ZONE sumo 150 150 {$zonesizes[5]} 0 0\n");
+			if ($numplayers <= 3 && )
+			{
+			print("SPAWN_ZONE sumo 150 150 {$zonesizes[1]} -.4 0 0\n");
+			}
+			elseif ($numplayers >= 4 && $numplayers <= 6)
+			{
+			print("SPAWN_ZONE sumo 150 150 {$zonesizes[2]} -.4 0 0\n");
+			}
+			elseif ($numplayers >= 7 && $numplayers <= 9)
+			{
+			print("SPAWN_ZONE sumo 150 150 {$zonesizes[3]} -.4 0 0\n");
+			}
+			elseif ($numplayers >= 10 && $numplayers <= 12)
+			{
+			print("SPAWN_ZONE sumo 150 150 {$zonesizes[4]} -.4 0 0\n");
+			}
+			elseif ($numplayers >= 13 && $numplayers <= 15)
+			{
+			print("SPAWN_ZONE sumo 150 150 {$zonesizes[5]} -.4 0 0\n");
+			}
 		}
 }
 ?>
