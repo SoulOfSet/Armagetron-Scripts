121beb3c6eb5347235df7a95e7aecea103522113
SoulOfSet <awesomewhitehot@yahoo.com>
2013-05-28 15:45:33 -0400

    Update lobby.php

--- lobby.php
+++ lobby.php
@@ -1,2 +1,13 @@
 #!/usr/bin/php
 <?php
+
+//The Available Servers with Ips
+$aAvailableServers = array(
+                            "rxeast" => "69.162.170.184:4537",
+                            "rxwest" => "96.44.136.41:4534",
+                            "rxgerm" => "176.9.218.179:4535",
+                            "afrohr" => "68.199.220.166:4536",
+                            
+                            
+                            ) 
+
