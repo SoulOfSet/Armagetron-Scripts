c53e176ce833f05c7b7ec72d49715eee8ddacd7f
SoulOfSet <awesomewhitehot@yahoo.com>
2013-06-23 10:41:53 -0500

    Update lobby.php

--- lobby.php
+++ lobby.php
@@ -7,6 +7,11 @@ $aAvailableServers = array(
                             "rxwest" => "96.44.136.41:4534",
                             "rxgerm" => "176.9.218.179:4535",
                             "afrohr" => "68.199.220.166:4536",
+                            "yellosub" => "62.75.170.138:4535",
+                            "megafortpro" => "tron.peatcoal.co.uk:4535",
+                            "hft" => "209.236.124.135:4536",
+                            
+                            
                             
                             
                             ) 
